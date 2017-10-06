6316cb246775c026e7ae5ba10cf8b5ad094ccc59
aybarscavus <aybars93@hotmail.de>
2017-10-06 09:37:55 +0200

    Timerfunktion eingebaut, LoginActivity erstellt

--- app/src/main/java/com/androidproject/hangman/MainActivity.java
+++ app/src/main/java/com/androidproject/hangman/MainActivity.java
@@ -1,7 +1,10 @@
 package com.androidproject.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTimer();
+    }
+
+    private void setTimer() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                changeActivity();
+            }
+        };
+        timer.schedule(timerTask, 5000);
+    }
+
+    private void changeActivity() {
+        System.out.println("TEST");
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 }
