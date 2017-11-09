f3379e84f2b90eddf6504e471d1ae67dcea8f9be
aybarscavus <aybars93@hotmail.de>
2017-11-09 10:19:28 +0100

    SettingsActivity hinzugefügt

--- app/src/main/java/com/androidproject/hangman/ChooseGameModeActivity.java
+++ app/src/main/java/com/androidproject/hangman/ChooseGameModeActivity.java
@@ -1,7 +1,10 @@
 package com.androidproject.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseGameModeActivity extends AppCompatActivity {
 
@@ -9,10 +12,25 @@ public class ChooseGameModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_game_mode);
+        initSettingsButton();
     }
 
     @Override
     public void onBackPressed() {
 
+    }
+
+    private void initSettingsButton() {
+        Button loginButton = (Button) findViewById(R.id.settings_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            changeToSettingsActivity();
+            }
+        });
+    }
+
+    private void changeToSettingsActivity() {
+        startActivity(new Intent(ChooseGameModeActivity.this, SettingsActivity.class));
     }
 }
