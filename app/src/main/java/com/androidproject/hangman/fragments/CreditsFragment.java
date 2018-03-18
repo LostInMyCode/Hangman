0a1445bf89ef25d6d1e276f5f2bd78e286de8071
LostInMyCode <tobias-sebastian@web.de>
2018-03-18 21:24:51 +0100

    if work.equals(android development): working = stop

--- app/src/main/java/com/androidproject/hangman/fragments/CreditsFragment.java
+++ app/src/main/java/com/androidproject/hangman/fragments/CreditsFragment.java
@@ -3,7 +3,7 @@ package com.androidproject.hangman.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -78,12 +78,6 @@ public class CreditsFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
